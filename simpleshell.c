00d87ba4c97398e48581a23c94f5c50cf3881abe
BrianSechelo <sechbrian123@gmail.com>
2023-12-07 14:46:26 -0800

    Task 2 (argc, argv)

--- simpleshell.c
+++ simpleshell.c
@@ -1,10 +1,21 @@
 #include "shell.h"
 /**
   * main - prompts user to enter input and executes user command
+  * @ac: arg count
+ * @av: arg vector
+ * 
+ * Return: 0 on success 1 on failure
  **/
-int main(void)
+int main(int ac, char **av)
 {
+	int j =0;
 	char command[128];
+	while(j < ac)
+	{
+		printf ("av[%d]:%s\n", j, av[j]); 
+                 ++j;
+	}
+	
 	while (1)
 	{
 		brian_prompt();
